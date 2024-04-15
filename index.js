a6258659d06b70e312358f517694b14d5e6222dd
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:18:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Q83
+// this function change string to upparcase or lowercase
+//this function change string to upparcase and lowercase
+function convertstring(str) {
+    var uppar = str.toUpperCase();
+    var lowwer = str.toLowerCase();
+    console.log("upparcase:", uppar);
+    console.log("lowercase:", lowwer);
+}
+;
+//change of case
+convertstring("Hello sadaf");
